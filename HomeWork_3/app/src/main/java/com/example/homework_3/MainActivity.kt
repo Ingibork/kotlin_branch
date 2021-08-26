e79dd25843c6f16b3193c5c510fdb2ecd72b174a
Ingibork <ingibork@mail.ru>
2021-08-26 19:57:06 +0300

    array 0.4 v

--- HomeWork_3/app/src/main/java/com/example/homework_3/MainActivity.kt
+++ HomeWork_3/app/src/main/java/com/example/homework_3/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.homework_3
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.homework_3.databinding.ActivityMainBinding
 import kotlin.random.Random
 
@@ -17,21 +18,61 @@ class MainActivity : AppCompatActivity() {
         jojo()
     }
 
-    fun jojo() {
-        val intent = Intent(this, MainActivity2::class.java)
-        var rnd = Random.nextInt(10000)
-        var rnd2 = Random.nextInt(10000)
-
-        bindingClass.b1.setOnClickListener {
-
-            if (rnd % 2 == 0 && rnd2 % 2 == 0) {
-                intent.putExtra("key", rnd)
-                intent.putExtra("key2", rnd2)
-                startActivity(intent)
-            } else {
-                rnd = Random.nextInt(10000)
-                rnd2 = Random.nextInt(10000)
-            }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 100 && resultCode == RESULT_OK && data != null) {
+            bindingClass.tv1.text = data.getIntExtra("key2", 1).toString()
         }
     }
+
+    fun jojo() {
+        val intent = Intent(this, MainActivity2::class.java)
+        bindingClass.b1.setOnClickListener {
+            var rnd =1
+            /**
+            var rnd2 =1
+            while (rnd2%2 !=0) {
+            rnd2 = Random.nextInt(10000)
+            }
+            intent.putExtra("key", rnd)
+            intent.putExtra("key2", rnd2)
+            startActivityForResult(intent, 100)
+             */
+            val s = arrayListOf<Int>()
+          //  var s = ar.toHashSet()
+          //  val result = mutableListOf<Int>()
+
+
+            var i = 0
+            while (i < 5) {
+            while (rnd%2 !=0) {
+            rnd = Random.nextInt(10000)
+            }
+            s.add(rnd)
+            i++
+            rnd = Random.nextInt(10000)
+
+            }
+            Log.e("myTag", s.toString())
+
+
+            intent.putIntegerArrayListExtra("key", s)
+            startActivityForResult(intent, 100)
+        }
+    }
+    /**
+    val rnd2 = Random.nextInt(10000)
+
+    while (index < fruitArray.size){
+    Log.d("MyLog","Fruit: ${fruitArray[index]}")
+    index++
+    }
+    if (rnd % 2 == 0 && rnd2 % 2 == 0) {
+    intent.putExtra("key", rnd)
+    intent.putExtra("key2", rnd2)
+    startActivityForResult(intent, 100)
+    } else {
+    jojo()
+    }
+     */
 }
--- HomeWork_3/app/src/main/java/com/example/homework_3/MainActivity2.kt
+++ HomeWork_3/app/src/main/java/com/example/homework_3/MainActivity2.kt
@@ -2,6 +2,8 @@ package com.example.homework_3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.example.homework_3.databinding.ActivityMain2Binding
 import kotlin.random.Random
 
@@ -12,12 +14,29 @@ class MainActivity2 : AppCompatActivity() {
         bindingClass2 = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(bindingClass2.root)
 
-        val message = intent.getIntExtra("key", 1)
-        val message2 = intent.getIntExtra("key2", 1)
 
-        bindingClass2.tv2.text = message.toString()
-        bindingClass2.tv3.text = message2.toString()
+        var message = intent.getIntegerArrayListExtra("key")!!
+        val aver = message.average()
+        Log.e("myTag", aver.toString())
 
+        bindingClass2.b2.setOnClickListener {
+            intent.putExtra("key2", aver)
+            setResult(RESULT_OK, intent, )
+            finish()
+
+
+        // val message2 = intent.getIntExtra("key2", 1)
+
+        // bindingClass2.tv2.text = message.toString()
+        // bindingClass2.tv3.text = message2.toString()
+    }
+
+    fun onClickMain(view: View)  {
+
+
+            // intent.putExtra("key", )
+        }
 
     }
+
 }
